3188c3878a0caeaca68f37848b9f86cd611660a1
HyeongJin Kim <khj7741@gmail.com>
2022-12-21 14:53:14 +0900

    style: Home Page Layout

---
+++ client/src/components/layout/ContentLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: aqua;
+`;
+
+const ContentLayout = (props) => {
+  return <ContentContainer></ContentContainer>;
+};
+
+export default ContentLayout;
