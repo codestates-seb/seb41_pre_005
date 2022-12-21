888405d1ae5c530c410cec4a555948815acc5dbe
HyeongJin Kim <khj7741@gmail.com>
2022-12-21 22:42:41 +0900

    style: #19 change border, shadow when input focused

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+const InputElement = styled.input`
+  width: 26.8rem;
+  height: 3.3rem;
+  border: 1px solid
+    ${(props) => (props.isFocused ? "hsl(210, 8%, 75%)" : "hsl(358,68%,59%)")};
+  border-radius: 3px;
 
-const Input = (props) => {
-  return <input />;
-
+  font-size: 1.6rem;
+  &:focus {
+    border: 1px solid
+      ${(props) => (props.isFocused ? "#38a9f0" : "hsl(358,68%,59%)")};
+    box-shadow: 0px 0px 0px 4px
+      ${(props) =>
+        props.isFocused
+          ? "hsla(206, 100%, 40%, 0.15)"
+          : "hsla(358,62%,47%,0.15)"};
+    outline: 0;
+  }
+`;
+const Input = ({}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const handleOnFocus = () => {
+    setIsFocused(true);
+  };
+  return <InputElement onFocus={handleOnFocus} isFocused={isFocused} />;
 };
 
 export default Input;
