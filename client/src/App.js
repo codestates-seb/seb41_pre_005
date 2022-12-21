6f38ff583ca3f925bd9a5a60bda62451c2e7fd77
HyeongJin Kim <khj7741@gmail.com>
2022-12-22 00:31:10 +0900

    style: #19 delete height property

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home";
 import styled from "styled-components";
+import SignUp from "./pages/user/SingUp";
 const PageLayout = styled.div`
   max-width: 126.4rem;
+  height: 100vh;
   margin: auto;
 `;
 function App() {
   return (
     <PageLayout>
-      <Home />
+      <SignUp />
     </PageLayout>
   );
 }
