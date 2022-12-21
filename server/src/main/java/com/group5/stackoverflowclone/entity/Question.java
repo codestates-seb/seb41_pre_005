8e552e27afee43a4e45296eefe7f978d720f11c0
강호수 <hosoo3513@gmail.com>
2022-12-21 17:18:41 +0900

    feat: entity 구현

---
+++ server/src/main/java/com/group5/stackoverflowclone/entity/Question.java
@@ -0,0 +1,73 @@
+package com.group5.stackoverflowclone.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long questionId;
+
+    private String title;
+
+    private String content;
+
+    private int clicks;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "question")
+    private List<QuestionTag> questionTags = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @OneToMany(mappedBy = "question")
+    private List<Answer> answers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "question")
+    private List<Vote> votes = new ArrayList<>();
+
+    public void addQuestionTag(QuestionTag questionTag) {
+        this.questionTags.add(questionTag);
+        if (questionTag.getQuestion() != this) {
+            questionTag.addQuestion(this);
+        }
+    }
+
+    public void addUser(User user) {
+        this.user = user;
+        if (!this.user.getQuestion().contains(this)) {
+            this.user.getQuestion().add(this);
+        }
+    }
+
+    public void addAnswer(Answer answer) {
+        this.answers.add(answer);
+        if (answer.getQuestion() != this) {
+            answer.addQuestion(this);
+        }
+    }
+
+    public void addQuestionTag(Vote vote) {
+        this.votes.add(vote);
+        if (vote.getQuestion() != this) {
+            vote.addQuestion(this);
+        }
+    }
+}
