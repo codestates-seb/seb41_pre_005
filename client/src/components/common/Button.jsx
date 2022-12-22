6b8aa8960e81b66aeeeb45899856f66fbca54d83
HyeongJin Kim <khj7741@gmail.com>
2022-12-23 00:16:33 +0900

    style: #27 Button width props

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 const ButtonElement = styled.button`
-  width: 26.8rem;
+  width: ${(props) => props.width || "26.8rem"};
   height: 3.7rem;
   background-color: #0693ff;
   color: white;
   font-size: 1.2rem;
   margin: auto;
 `;
-const Button = ({ children }) => {
-  return <ButtonElement>{children}</ButtonElement>;
+const Button = ({ children, width }) => {
+  return <ButtonElement width={width}>{children}</ButtonElement>;
 };
 
 export default Button;
