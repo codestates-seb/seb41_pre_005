356f6114a7fe24ffccbd49e5b6aabb9133bc700b
HyeongJin Kim <khj7741@gmail.com>
2022-12-23 00:17:57 +0900

    style: #27 modify identifier

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,30 +1,33 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import AlertIcon from "../login/AlertWarning";
 const InputElement = styled.input`
-  width: 26.8rem;
+  width: ${(props) => props.width || "26.8rem"};
   height: 3.3rem;
   border: 1px solid
-    ${(props) => (props.isFocused ? "hsl(210, 8%, 75%)" : "hsl(358,68%,59%)")};
+    ${(props) => (props.isValid ? "hsl(210, 8%, 75%)" : "hsl(358,68%,59%)")};
   border-radius: 3px;
-
   font-size: 1.6rem;
+
   &:focus {
     border: 1px solid
-      ${(props) => (props.isFocused ? "#38a9f0" : "hsl(358,68%,59%)")};
+      ${(props) => (props.isValid ? "#38a9f0" : "hsl(358,68%,59%)")};
     box-shadow: 0px 0px 0px 4px
       ${(props) =>
-        props.isFocused
+        props.isValid
           ? "hsla(206, 100%, 40%, 0.15)"
           : "hsla(358,62%,47%,0.15)"};
     outline: 0;
   }
 `;
-const Input = ({}) => {
-  const [isFocused, setIsFocused] = useState(false);
+const Input = ({ width }) => {
+  const [isValid, setIsValid] = useState(true);
   const handleOnFocus = () => {
-    setIsFocused(true);
+    setIsValid(true);
   };
-  return <InputElement onFocus={handleOnFocus} isFocused={isFocused} />;
+  return (
+    <InputElement width={width} onFocus={handleOnFocus} isValid={isValid} />
+  );
 };
 
 export default Input;
