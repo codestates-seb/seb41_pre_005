e6bcf1f27df192fa402ba67cc3a83d585bd79304
HyeongJin Kim <khj7741@gmail.com>
2022-12-23 10:07:18 +0900

    style: etc

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import styled from "styled-components";
 import SignUp from "./pages/user/SingUp";
 import Login from "./pages/user/Login";
+import HeaderLayout from "./components/layout/HeaderLayout";
+import Questions from "./pages/question/Questions";
 const PageLayout = styled.div`
   max-width: 126.4rem;
   height: 100vh;
@@ -11,7 +13,8 @@ const PageLayout = styled.div`
 function App() {
   return (
     <PageLayout>
-      <Login />
+      <HeaderLayout />
+      <Questions />
     </PageLayout>
   );
 }
--- client/src/components/layout/MainContentLayout.jsx
+++ client/src/components/layout/MainContentLayout.jsx
@@ -3,10 +3,9 @@ import styled from "styled-components";
 const MainContainer = styled.div`
   width: 100%;
   height: 100%;
-  background-color: blueviolet;
 `;
-const MainContentLayout = (props) => {
-  return <MainContainer></MainContainer>;
+const MainContentLayout = ({ children }) => {
+  return <MainContainer>{children}</MainContainer>;
 };
 
 export default MainContentLayout;
