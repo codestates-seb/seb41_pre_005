5ab5f09c5c0ebb349605dc50124437003c0682f2
psc <whirled7@gmail.com>
2022-12-23 20:02:12 +0900

    feat QuestionDetail Button(Vote Edit etc..)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Home from "./pages/Home";
 import styled from "styled-components";
-import SignUp from "./pages/user/SingUp";
-import Login from "./pages/user/Login";
-import HeaderLayout from "./components/layout/HeaderLayout";
-import Questions from "./pages/question/Questions";
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
+// import SignUp from "./pages/user/SingUp";
+// import Login from "./pages/user/Login";
+// import HeaderLayout from "./components/layout/HeaderLayout";
+// import Questions from "./pages/question/Questions";
+// import Header from "./components/common/Header";
+// import Footer from "./components/common/Footer";
+import Profile from "./pages/user/Profile";
 
 const PageLayout = styled.div`
   max-width: 126.4rem;
@@ -17,8 +18,7 @@ function App() {
   return (
     <>
       <PageLayout>
-        <Header />
-        <Home />
+        <Profile />
       </PageLayout>
     </>
   );
