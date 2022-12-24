d5148cb88aab7f51175ded551b1305329d4aa0a6
HyeongJin Kim <khj7741@gmail.com>
2022-12-24 20:18:43 +0900

    style: #21 LeftSideLayout fixed 제거

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import SignUp from "./pages/user/SingUp";
 import Login from "./pages/user/Login";
 // import HeaderLayout from "./components/layout/HeaderLayout";
-// import Questions from "./pages/question/Questions";
+import Questions from "./pages/question/Questions";
 // import Header from "./components/common/Header";
 // import Footer from "./components/common/Footer";
 import Profile from "./pages/user/Profile";
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       <PageLayout>
-        <SignUp></SignUp>
+        <Questions />
       </PageLayout>
     </>
   );
