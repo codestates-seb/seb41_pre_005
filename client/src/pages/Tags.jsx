fe02116b60dd1c6d372a9979b2bcc601b10b15a8
psc <whirled7@gmail.com>
2022-12-27 11:05:57 +0900

    style : LoginHeader DropDown Container

--- client/src/pages/Tags.jsx
+++ client/src/pages/Tags.jsx
@@ -4,7 +4,6 @@ import ContentLayout from "../components/layout/ContentLayout";
 import LeftSideLayout from "../components/layout/LeftSideLayout";
 import MainContentLayout from "../components/layout/MainContentLayout";
 import { TagsHeadline } from "../components/common/HeadLine";
-import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
 import TagsInput from "../components/tags/TagsInput";
 
