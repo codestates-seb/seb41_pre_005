69c4bc5877db30d5d363a8173620e41471d49720
HyeongJin Kim <khj7741@gmail.com>
2023-01-02 12:49:38 +0900

    feat: #91 display Searched data

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ContentLayout from "../components/layout/ContentLayout";
 import LeftSideLayout from "../components/layout/LeftSideLayout";
 import MainContentLayout from "../components/layout/MainContentLayout";
 import QuestionsLayout from "../components/layout/question/QuestionsLayout";
 import SearchNav from "../components/searchResults/SearchNav";
-import QuestionsList from "../components/questions/QuestionsList";
 import Footer from "../components/common/Footer";
 import { SearchResultsHeadLine } from "../components/common/HeadLine";
+import { useLocation } from "react-router-dom";
+import { SearchQuestion } from "../api/questionAPI";
+import { useDispatch } from "react-redux";
+import { storeQuestions } from "../redux/questionsReducer";
+import SearchList from "../components/search/SearchList";
 
 const SearchResults = (props) => {
   const QuestionsHeadLayout = styled.div`
     margin-bottom: 1.2rem;
   `;
+  const location = useLocation();
+  const searchTarget = location.state.search;
+  const [questions, setQuestions] = useState();
 
+  useEffect(() => {
+    async function search() {
+      const res = await SearchQuestion(searchTarget);
+      setQuestions(res);
+    }
+    search();
+  }, [searchTarget]);
   return (
     <>
       <QuestionsLayout>
@@ -24,7 +38,7 @@ const SearchResults = (props) => {
               <SearchResultsHeadLine />
             </QuestionsHeadLayout>
             <SearchNav />
-            <QuestionsList />
+            <SearchList questions={questions} />
           </MainContentLayout>
         </ContentLayout>
       </QuestionsLayout>
